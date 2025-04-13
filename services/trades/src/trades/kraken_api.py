a850f7d33961433c075d6f743a86ff8683280ad1
moreshwarnabar <mrnabar@gmail.com>
2025-04-13 14:59:21 -0700

    pushes real data from Kraken to kafka topic

---
+++ services/trades/src/trades/kraken_api.py
@@ -0,0 +1,73 @@
+import json
+from loguru import logger
+from pydantic import BaseModel
+from websocket import create_connection
+
+class Trade(BaseModel):
+    product_id: str
+    price: float
+    quantity: float
+    timestamp: str
+
+    def to_dict(self) -> dict:
+        return self.model_dump()
+
+class KrakenAPI:
+
+    URL = 'wss://ws.kraken.com/v2'
+
+    def __init__(self, product_ids: list[str]):
+        self.product_ids = product_ids
+
+        self._ws_client = create_connection(self.URL)
+        self._subscribe()
+
+    def get_trades(self) -> list[Trade]:
+        data = self._ws_client.recv()
+
+        if "heartbeat" in data:
+            logger.info("Heartbeat received")
+            return []
+        
+        try:
+            data = json.loads(data)
+        except json.JSONDecodeError as e:
+            logger.error(f"Error decoding JSON: {e}")
+            return []
+        
+        try:
+            trades_data = data['data']
+        except KeyError as e:
+            logger.error(f"No 'data' field in the message: {e}")
+            return []
+        
+        trades = [
+            Trade(
+                product_id=trade['symbol'],
+                price=float(trade['price']),
+                quantity=float(trade['qty']),
+                timestamp=trade['timestamp']
+            )
+            for trade in trades_data
+        ]
+
+        return trades
+
+    def _subscribe(self):
+        self._ws_client.send(
+            json.dumps({
+                "method": "subscribe",
+                "params": {
+                    "channel": "trade",
+                    "symbol": self.product_ids,
+                    "snapshot": False
+                }
+            })
+        )
+
+        for _ in self.product_ids:
+            _ = self._ws_client.recv()
+            _ = self._ws_client.recv()
+
+    def _on_message(self, message):
+        pass
---
+++ services/trades/src/trades/main.py
@@ -0,0 +1,44 @@
+from loguru import logger
+from quixstreams import Application
+
+from kraken_api import KrakenAPI, Trade
+
+def run(
+    broker_address: str,
+    kafka_topic_name: str,
+    kraken_api: KrakenAPI
+):
+    app = Application(
+        broker_address=broker_address
+    )
+
+    topic = app.topic(name=kafka_topic_name, value_serializer='json')
+
+    with app.get_producer() as producer:
+
+        while True:
+            events: list[Trade] = kraken_api.get_trades()
+            for event in events:
+                message = topic.serialize(
+                    # key=event["id"], 
+                    value=event.to_dict()
+                    )
+
+                producer.produce(
+                    topic=topic.name, 
+                    value=message.value, 
+                    key=message.key
+                )
+
+                logger.info(f"Produced message to topic: {topic.name}")
+
+            import time
+            time.sleep(1)
+
+if __name__ == "__main__":
+    api = KrakenAPI(product_ids=['BTC/EUR'])
+    run(
+        broker_address='localhost:31234',
+        kafka_topic_name='trades',
+        kraken_api=api
+    )
