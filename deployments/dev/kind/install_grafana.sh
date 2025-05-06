c331e657a3f6ea8c74f1bf825c98071f6d88f0be
moreshwarnabar <mrnabar@gmail.com>
2025-05-05 19:12:20 -0700

    adds script to install grafana

---
+++ deployments/dev/kind/install_grafana.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+helm repo add grafana https://grafana.github.io/helm-charts
+
+helm upgrade --install --create-namespace --wait grafana grafana/grafana --namespace=monitoring --values manifests/grafana-values.yaml
